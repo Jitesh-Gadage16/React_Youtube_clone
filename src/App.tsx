e90b13a5de66de60256043a1bf89f64fc182293b
Jitesh-Gadage16 <gadagejitesh16@gmail.com>
2025-01-09 00:50:15 -0500

    folder structure created

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { RouterProvider } from "react-router-dom";
+import routes from "./routes/routes";
+import { ThemeContext } from "./theme/themeContext";
+import { useState } from "react";
+import Theme from "./theme/Theme";
+import store from "./store/store";
+import { Provider } from "react-redux";
+
+function App() {
+  // handling theme switching dark <-> light
+  const [currentTheme, setCurrentTheme] = useState("light");
+  const toggleCurrentTheme = () => {
+    if (currentTheme === "light") {
+      setCurrentTheme("dark");
+    }
+    if (currentTheme === "dark") {
+      setCurrentTheme("light");
+    }
+  };
+
+  return (
+    <ThemeContext.Provider value={{ currentTheme, toggleCurrentTheme }}>
+      <Theme>
+        {/* main app logic */}
+        <Provider store={store}>
+          <RouterProvider router={routes} />
+        </Provider>
+      </Theme>
+    </ThemeContext.Provider>
+  );
+}
+
+export default App;
